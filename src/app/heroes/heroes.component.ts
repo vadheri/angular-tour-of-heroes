e00d7ed56eb4e29b9fbc594b8c5f4c55f5336f99
Dipak <jayesh.sankhala@prospecta.com>
2022-11-12 17:04:34 +0530

    changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -41,7 +41,7 @@ const TREE_DATA: CourseNode[] = [
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = {
